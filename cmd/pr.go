8e6ee546317ef2be79319b7798c2ef136b617b4b
bpicode <bjoern.pirnay@googlemail.com>
2017-11-12 18:35:05 +0100

    git push and pr

--- cmd/pr.go
+++ cmd/pr.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"os"
 	"os/exec"
+	"strings"
+	"fmt"
+	"io/ioutil"
 
+	"github.com/google/go-github/github"
 	"github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"io/ioutil"
+	"golang.org/x/oauth2"
 )
 
 var prCmd = &cobra.Command{
@@ -16,125 +20,199 @@ var prCmd = &cobra.Command{
 	RunE: pr,
 }
 
-var gopath string
-
-var branch = "auto-upgrade-dependencies"
-
-var workingDir string
-
 func init() {
 	RootCmd.AddCommand(prCmd)
 }
 
 func pr(cmd *cobra.Command, _ []string) error {
-	project := cmd.Flag("project").Value.String()
-	if project == "" {
-		return errors.New("required flag 'repo' not set")
-	}
-
 	tmp, err := ioutil.TempDir("", "depbot")
 	if err != nil {
 		return errors.Wrapf(err, "cannot create temporary directory with prefix '%s'", "depbot")
 	}
 	defer os.Remove(tmp)
-	gopath = tmp
+	ctx, err := createContext(cmd, tmp)
+	if err != nil {
+		return err
+	}
 
-	command("mkdir", gopath).Run()
-	workingDir = gopath + "/src/" + project
+	ctx.pwd = ctx.gopath + "/src/" + ctx.project
+	mkWoringDir(ctx.pwd)
+
+	err = clone(ctx)
+	if err != nil {
+		return errors.Wrapf(err, "cannot clone '%s'", ctx.project)
+	}
+
+	err = depUpdate(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot update")
+	}
+
+	err = checkoutBranch(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot checkout")
+	}
+
+	err = gitCommit(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot commit")
+	}
+
+	err = fork(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot fork")
+	}
+
+	err = gitPush(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot push")
+	}
+
+	err = pullRequest(ctx)
+	if err != nil {
+		return errors.Wrap(err, "cannot create pull request")
+	}
+	return nil
+}
+
+func mkWoringDir(workingDir string) {
+	mkdir := exec.CommandContext(context.Background(), "mkdir", "-p", workingDir)
+	mkdir.Stdout = os.Stdout
+	mkdir.Stderr = os.Stderr
+	mkdir.Run()
 	logrus.Infof("Working directory is '%s'", workingDir)
-
-	_ = checkoutMaster()
-
-	err = goGet(project)
-	if err != nil {
-		return errors.Wrapf(err, "cannot get project '%s'", project)
-	}
-
-	err = checkoutMaster()
-	if err != nil {
-		logrus.Warn("Reset to master branch failed", err)
-	}
-
-	err = depUpdate()
-	if err != nil {
-		return errors.Wrapf(err, "cannot update dependencies of project '%s'", project)
-	}
-
-	// git push
-	// create PR
-	err = checkoutBranch()
-	if err != nil {
-		return errors.Wrap(err, "cannot checkout branch")
-	}
-
-	err = gitCommit()
-	if err != nil {
-		return errors.Wrap(err, "cannot commit changes")
-	}
-
-	err = gitPush()
-	return err
 }
 
-func gitPush() error {
-	logrus.Infof("Deleting upstream")
-	command("git", "push", "origin", "--delete", "auto-upgrade-dependencies").Run()
-	logrus.Infof("Pushing changes")
-	err := command("git", "push", "--set-upstream", "origin", branch).Run()
-	return errors.Wrap(err, "failed to push to remote repository")
+func pullRequest(ctx *projectCtx) error {
+	client := ctx.githubClient()
+
+	title := "Automatic upgrade of dependencies by depbot"
+	head := fmt.Sprintf("%s:%s", ctx.user, ctx.featurebranch)
+	base := "master"
+	body := "Bot upgrade of dependencies using dep. Please review carefully. I will not answer to review comments."
+
+	_, _, err := client.PullRequests.Create(context.Background(), ctx.owner, ctx.repo, &github.NewPullRequest{
+		Title: &title,
+		Head:  &head,
+		Base:  &base,
+		Body:  &body,
+	})
+	return errors.Wrap(err, "could not create pull request")
+}
+func gitPush(ctx *projectCtx) error {
+	err := ctx.command("git", "push", "fork").Run()
+	return errors.Wrap(err, "could not push")
 }
 
-func gitCommit() error {
+func fork(ctx *projectCtx) error {
+	client := ctx.githubClient()
+	_, err := client.Repositories.Delete(context.Background(), ctx.user, ctx.repo)
+	if err != nil {
+		logrus.Infof("Could not delete remote fork: %s", err)
+	}
+
+	_, _, err = client.Repositories.CreateFork(context.Background(), ctx.owner, ctx.repo, &github.RepositoryCreateForkOptions{})
+	if err != nil {
+		logrus.Infof("Could not fork (sometimes you may ignore this error): %s", err)
+	}
+
+	err = ctx.command("git", "remote", "add", "fork",
+		fmt.Sprintf("https://%s:%s@github.com/%s/%s", ctx.user, ctx.token, ctx.user, ctx.repo)).Run()
+	if err != nil {
+		return errors.Wrap(err, "cannot add fork as remote")
+	}
+	return nil
+}
+
+func gitCommit(ctx *projectCtx) error {
 	logrus.Infof("Committing changes")
-	command("git", "config", "user.email", "depbot@yandex.com").Run()
-	command("git", "config", "user.name", "dep-bot").Run()
-	err := command("git", "add", "--all").Run()
+	ctx.command("git", "config", "user.email", ctx.email).Run()
+	ctx.command("git", "config", "user.name", ctx.user).Run()
+	err := ctx.command("git", "add", "--all").Run()
 	if err != nil {
 		return errors.Wrap(err, "git add --all failed")
 	}
 
-	err = command("git", "commit", "--message=automatic upgrade of 3rd party dependencies").Run()
+	err = ctx.command("git", "commit", "--message=automatic upgrade of 3rd party dependencies").Run()
 	return errors.Wrap(err, "git commit failed")
 }
 
-func depUpdate() error {
+func depUpdate(ctx *projectCtx) error {
 	logrus.Infof("Updating dependencies")
-	err := command("dep", "ensure", "-update").Run()
+	err := ctx.command("dep", "ensure", "-update").Run()
 	return errors.Wrap(err, "dep ensure -update failed")
 }
 
-func checkoutBranch() error {
-	logrus.Infof("Switch to branch '%s'", branch)
-	err := command("git", "checkout", "-b", branch).Run()
+func checkoutBranch(ctx *projectCtx) error {
+	logrus.Infof("Switch to branch '%s'", ctx.featurebranch)
+	err := ctx.command("git", "checkout", "-b", ctx.featurebranch).Run()
 	if err != nil {
 		logrus.Warnf("Checkout of new branch failed: %v", err)
 	}
-	err = command("git", "checkout", branch).Run()
-	return errors.Wrapf(err, "could not switch to a branch '%s'", branch)
+	err = ctx.command("git", "checkout", ctx.featurebranch).Run()
+	return errors.Wrapf(err, "could not switch to a branch '%s'", ctx.featurebranch)
 }
 
-func goGet(project string) error {
-	logrus.Infof("Go-getting '%s'", project)
-	c := command("go", "get", "-u", project)
-	c.Dir = gopath
+func clone(ctx *projectCtx) error {
+	logrus.Infof("Cloning '%s'", ctx.project)
+	c := ctx.command("git", "clone",
+		fmt.Sprintf("https://%s:%s@%s", ctx.user, ctx.token, ctx.project), ".")
 	return c.Run()
 }
 
-func checkoutMaster() error {
-	logrus.Infof("Switching to master branch")
-	err := command("git", "checkout", "master").Run()
-	if err != nil {
-		return errors.Wrap(err, "cannot checkout master branch")
-	}
-	command("git", "branch", "-D", "auto-upgrade-dependencies").Run()
-	return nil
+type projectCtx struct {
+	project       string
+	token         string
+	user          string
+	email         string
+	pwd           string
+	repo          string
+	owner         string
+	featurebranch string
+	gopath        string
 }
 
-func command(name string, arg ...string) *exec.Cmd {
+func createContext(cmd *cobra.Command, gopath string) (*projectCtx, error) {
+	ctx := new(projectCtx)
+	ctx.gopath = gopath
+
+	ctx.user = "dep-bot"
+	ctx.email = "depbot@yandex.com"
+
+	project := cmd.Flag("project").Value.String()
+	if project == "" {
+		return nil, errors.New("required flag 'repo' not set")
+	}
+	ctx.project = project
+	token := cmd.Flag("token").Value.String()
+	if project == "" {
+		return nil, errors.New("required flag 'token' not set")
+	}
+	ctx.token = token
+
+	projectPaths := strings.Split(ctx.project, "/")
+	ctx.repo = projectPaths[len(projectPaths)-1]
+	ctx.owner = projectPaths[1]
+
+	ctx.featurebranch = "auto-upgrade-dependencies"
+
+	return ctx, nil
+}
+
+func (ctx *projectCtx) command(name string, arg ...string) *exec.Cmd {
 	c := exec.CommandContext(context.Background(), name, arg...)
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
-	c.Dir = workingDir
-	c.Env = append(os.Environ(), "GOPATH="+gopath)
+	c.Dir = ctx.pwd
+	c.Env = append(os.Environ(), "GOPATH="+ctx.gopath)
 	return c
+}
+
+func (ctx *projectCtx) githubClient() *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: ctx.token},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+	client := github.NewClient(tc)
+	return client
 }
