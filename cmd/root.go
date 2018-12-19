f17a764d1f8739452e7f37d2b91aa1db81446356
bpicode <bjoern.pirnay@googlemail.com>
2018-12-19 11:25:36 +0100

    Define flags

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,8 @@ func init() {
 	RootCmd.PersistentFlags().String("token", "", "github token")
 	RootCmd.PersistentFlags().String("user", "dep-bot", "github user name")
 	RootCmd.PersistentFlags().String("mail", "depbot@yandex.com", "github user email")
+	RootCmd.PersistentFlags().String("mode", "dep", "dep or gomodule")
+	RootCmd.PersistentFlags().String("raise", "", "what part of the version to raise: patch")
 	RootCmd.InitDefaultHelpFlag()
 	RootCmd.InitDefaultHelpCmd()
 }
